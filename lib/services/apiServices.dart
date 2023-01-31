f3211da644a534546df8c93a80331c09f042baea
princeGedeon <guedjegedeon03@gmail.com>
2023-01-31 18:28:50 +0100

    functionality change password

--- lib/services/apiServices.dart
+++ lib/services/apiServices.dart
@@ -7,41 +7,76 @@ import 'package:ui_event_app/constants/constants_api.dart';
 
 import '../utils/helper_preferences.dart';
 
-class ApiServices{
-
-  static Future<bool> connect(String email,String password) async{
+class ApiServices {
+  static Future<bool> connect(String email, String password) async {
     var dio = Dio();
+    try {
+      final response = await dio.post(APiConstants.BASEURL + "api/user/login/",
+          data: {'email': email, "password": password});
+
+      Map<String, dynamic> data = response.data;
+
+      if (response.statusCode != 400) {
+        HelperPreferences.saveStringValue(
+            "accessToken", data["token"]["access"].toString());
+        HelperPreferences.saveStringValue(
+            "refreshToken", data["token"]["refresh"].toString());
+
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+
+  static Future<bool> changepassword(String password,String password2,String token,String uid) async {
+    var dio=Dio();
     try{
-      final response = await dio.post(APiConstants.BASEURL+"api/user/login/",
-          data: {'email':email,"password":password});
+      final response=await dio.post(APiConstants.BASEURL+"api/user/reset-password/${uid}/${token}/",
 
-      Map<String,dynamic> data= response.data;
-
+         data: {
+        "password":password,
+        "password2":password2
+      });
+      print(response.statusCode);
       if (response.statusCode!=400){
-
-        HelperPreferences.saveStringValue("accessToken",data["token"]["access"].toString());
-        HelperPreferences.saveStringValue("refreshToken",data["token"]["refresh"].toString());
-
-
-
-
-
-
         return true;
       }
       else{
         return false;
       }
-
     }
     catch (e){
       print(e.toString());
       return false;
     }
-
-
-
-    
   }
 
+  static Future<bool> sendpassmail(String email) async{
+    var dio=Dio();
+    try{
+
+      final response=await dio.post(APiConstants.BASEURL+"api/user/send-reset-password-mail/",data: {
+        'email':email
+      });
+      print(response.statusCode);
+
+      if (response.statusCode != 400) {
+
+
+        return true;
+      } else {
+
+        return false;
+      }
+
+    }
+    catch(e){
+      print(e.toString());
+      return false;
+    }
+  }
 }
